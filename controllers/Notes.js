81d16edcd7a3e2d351ab8e5b3e3454899318ebc1
Alan Alconso <alnsanrik360@gmail.com>
2022-06-26 20:52:45 -0500

    Agregando una autenticacion al crear una nota

--- controllers/Notes.js
+++ controllers/Notes.js
@@ -2,6 +2,7 @@ const notesRouter = require('express').Router()
 const Note = require('../models/Note')
 const User = require('../models/User')
 const handleErrors = require('../middleware/handleErrors')
+const jwt = require('jsonwebtoken')
 
 notesRouter.get('/', async (request, response) => { 
     const notes = await Note.find({}).populate('user', { username: 1, name: 1 })
@@ -10,15 +11,28 @@ notesRouter.get('/', async (request, response) => {
 })
 
 
+const getTokenFrom = request => { // encabezado de authorization aislado
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 notesRouter.post('/', async (request, response) => { 
-  const {body} = request
+  const body = request.body
   if (!body || !body.content){
     return response.status(400).json({
       error : "note.content is missing"
     })
   }
 
-  const user = await User.findById(body.userId)
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET) // comprueba la validez del token
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const note = new Note({
     content: body.content,
@@ -30,6 +44,7 @@ notesRouter.post('/', async (request, response) => {
   const savedNote = await note.save()
   user.notes = user.notes.concat(savedNote._id)
   await user.save()
+  console.log(savedNote)
   
   response.json(savedNote)
 })
--- middleware/handleErrors.js
+++ middleware/handleErrors.js
@@ -1,6 +1,18 @@
 module.exports = (error, request, response, next) => {
     console.log(error)
   
-    if(error.name === 'CastError') response.status(404).send({error: 'No hay nada'})
-    else response.status(500).end()
+    if (error.name === 'CastError') {
+      return response.status(400).send({
+        error: 'malformatted id'
+      })
+    } else if (error.name === 'ValidationError') {
+      return response.status(400).json({
+        error: error.message 
+      })
+    } else if (error.name === 'JsonWebTokenError') {
+      return response.status(401).json({
+        error: 'invalid token'
+      })
+    }
+    next(error)
   }
